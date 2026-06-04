710cf0d38cbb05bd6e013eeddef00a83115162db
Fahmida Faiza <fahmidabintegolam@gmail.com>
2026-06-04 21:39:39 +0600

    Add files via upload
    Add files

---
+++ script.js
@@ -0,0 +1,24 @@
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
+
+function clock() {
+
+    let date = new Date();
+
+    let hr = date.getHours() % 12;
+    let mint = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hour.style.transform =
+        `rotateZ(${hr * 30 + mint * 0.5}deg)`;
+
+    minute.style.transform =
+        `rotateZ(${mint * 6 + sec * 0.1}deg)`;
+
+    second.style.transform =
+        `rotateZ(${sec * 6}deg)`;
+}
+
+clock();
+setInterval(clock, 1000);
